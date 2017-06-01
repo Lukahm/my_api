cdcfd7322e6fb98bb651ecb8e3ccc542efc68814
Haomin Liu <karliuhaomin@hotmail.com>
2017-06-02 02:09:50 +1200

    add animals routes for api version 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      get     "/animals",     to: "animals#index"
+      post    "/animals",     to: "animals#create"
+      get     "/animals/:id", to: "animals#show"
+      put     "/animals/:id", to: "animals#update"
+      delete  "/animals/:id", to: "animals#destroy"
+    end
+  end
+
 end
